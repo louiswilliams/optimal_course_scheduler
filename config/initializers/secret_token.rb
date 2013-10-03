05d2860a61f8cdbd203e9f2664ce7b2b0e965d3c
Louis <louieaw@gmail.com>
2013-10-02 22:09:07 -0400

    Created Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OptimalCourseScheduler::Application.config.secret_key_base = '83fccfcba0e4235fec9f677762b322f51023b0e634e610dd029f711b359e541ef5f6a0d445821fd3735cacf6c823bef466d20c1026e134cc0e8e11326db7c245'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OptimalCourseScheduler::Application.config.session_store :cookie_store, key: '_optimal_course_scheduler_session'
