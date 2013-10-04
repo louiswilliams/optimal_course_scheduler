032af349e2bd6b1444c9acacdad5ff39ed37cdc8
Louis <louieaw@gmail.com>
2013-10-04 00:10:39 -0400

    Completed JSON parser that inserts course data into tables

--- data/parse_json.rb
+++ data/parse_json.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'json'
+require 'sqlite3'
 
 ##
 ## This section of the code just parses the two JSON files to create
@@ -9,6 +10,7 @@ require 'json'
 # Assuming these are your filenames
 courseFile = File.new("courses.json","r")
 sectionFile = File.new("sections.json","r")
+
 
 rawCourses = ""
 rawSections = ""
@@ -37,10 +39,11 @@ sections.each do |section|
     # If a day of week is found in meeting "days" array, then set the coulmn
     # value to true, otherwise, false.
     weekdays.each do |key,value|
-      meeting["#{value}"] = (meeting["days"].index(key)) ? true : false
+      meeting["#{value}"] = (meeting["days"].index(key)) ? "true" : "false"
     end
     # Adds the section's id to the array
-    meeting["section"] = section["id"]
+    meeting["section_id"] = section["id"]
+    meeting["course_id"] = section["course_id"]
     meetings << meeting
   end
 end
@@ -53,3 +56,35 @@ end
 ## This section of the code imports the three created hashmaps as tables
 ##
 
+# devtest.db is just a copy of my dev "db/development.sqlite3".
+db = SQLite3::Database.open("devtest.sqlite3")
+
+# Add courses
+
+#courses.each do |course|
+#  db.execute("insert into courses (course_id,credits,college,title) values(?,?,?,?)",
+#    course["id"],course["credit"],course["college"],course["title"])
+#end
+
+#sections.each do |section|
+#  db.execute("insert into sections (section_id,course_id) values(?,?)",
+#    section["id"],db.execute("select id from courses where course_id=\'#{section["course_id"]}\'"))
+#end
+
+meetings.each do |meeting|
+  id_course = db.execute("select id from courses where course_id=\'#{meeting["course_id"]}\' limit 1")[0][0]
+  id_section = db.execute("select id from sections where course_id=\'#{id_course}\' limit 1")[0][0]
+#  puts "#{meeting["course_id"]}: #{meeting["section_id"]}, #{id_course}: #{id_section}"
+  db.execute("insert into meetings (section_id,course_id,location,start_time,end_time," +
+  "monday,tuesday,wednesday,thursday,friday) values(?,?,?,?,?,?,?,?,?,?)",
+    id_section,
+    id_course,
+    meeting["location"],
+    Time.parse(meeting["start_time"]).strftime("%H:%M:%S"),
+    Time.parse(meeting["end_time"]).strftime("%H:%M:%S"),
+    meeting["monday"],
+    meeting["tuesday"],
+    meeting["wednesday"],
+    meeting["thursday"],
+    meeting["friday"])
+end
